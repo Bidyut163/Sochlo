8fe7a4130b139f4f891f8d48c36a50132907da64
bidyut <bideka03@in.ibm.com>
2019-10-05 18:27:44 +0530

    add port for local server

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-var bodyParser          = require("body-parser"),
-    methodOverride      = require("method-override"),
-    expressSanitizer    = require("express-sanitizer"),
-    mongoose            = require("mongoose"),
-    passport            = require("passport"),
-    localStrategy       = require("passport-local"),
-    User                = require("./models/user"),
-    flash               = require("connect-flash"),
-    express             = require("express"),
-    app                 = express();
-    
+var bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  expressSanitizer = require("express-sanitizer"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
+  User = require("./models/user"),
+  flash = require("connect-flash"),
+  express = require("express"),
+  app = express();
+
 var blogRoutes = require("./routes/blogs"),
-    commentRoutes = require("./routes/comments"),
-    indexRoutes = require("./routes/index");
+  commentRoutes = require("./routes/comments"),
+  indexRoutes = require("./routes/index");
 
 // APP CONFIG
-var url =process.env.DATABASEURL || "mongodb://localhost/abhilasha";
+var url = process.env.DATABASEURL || "mongodb://localhost/abhilasha";
 mongoose.connect(url);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 app.use(flash());
 
-
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "I am the funniest person alive!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 //Current user config
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 // RESTFUL ROUTES
-app.use("/blogs",blogRoutes);
-app.use("/blogs/:id/comments",commentRoutes);
-app.use("/",indexRoutes);
+app.use("/blogs", blogRoutes);
+app.use("/blogs/:id/comments", commentRoutes);
+app.use("/", indexRoutes);
 
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("SERVER IS RUNNING!");
+app.listen(PORT, process.env.IP, function() {
+  console.log("SERVER IS RUNNING! at port", PORT);
 });
