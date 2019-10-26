75071ec570df4283869dfea6a47ccfc2a96e2df8
bidyut <bideka03@in.ibm.com>
2019-10-27 01:51:06 +0530

    newly added

---
+++ routes/admin.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var Blog = require("../models/blog");
+
+router.get("/dashboard", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    err ? console.log(err) : res.render("admin/dashboard", { blogs: blogs });
+  });
+});
+
+router.get("/posts", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    err ? console.log(err) : res.render("admin/posts", { blogs: blogs });
+  });
+});
+
+router.get("/catagories", (req, res) => {
+  res.render("admin/catagories");
+});
+
+router.get("/users", (req, res) => {
+  res.render("admin/users");
+});
+
+router.get("/login", (req, res) => {
+  res.render("admin/login");
+});
+
+module.exports = router;
