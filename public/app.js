fdd6b28f788cd4b141eaf74f30e2a92e93ca3a7c
bidyut <bideka03@in.ibm.com>
2019-10-27 01:51:38 +0530

    no change

--- public/app.js
+++ public/app.js
@@ -1,6 +1,26 @@
-var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months = [ "January", "February", "March", "April", "May", "June",
-               "July", "August", "September", "October", "November", "December" ];
+var weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 var date = new Date();
 
 document.querySelector("#day").innerHTML = date.getDate();
