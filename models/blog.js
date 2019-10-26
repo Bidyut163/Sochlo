4e28eb38babbfc374496c75322673a32a1be4fb3
bidyut <bideka03@in.ibm.com>
2019-10-27 02:24:24 +0530

    category is added

--- models/blog.js
+++ models/blog.js
@@ -1,21 +1,22 @@
 var mongoose = require("mongoose");
 var blogSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {type: Date, default: Date.now},
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-      username: String
+  title: String,
+  image: String,
+  category: String,
+  body: String,
+  created: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Comment"
-        }    
-    ]
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 module.exports = mongoose.model("Blog", blogSchema);
